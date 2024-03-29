7b61dc66be0456c7ed6c5b651ea3bb6c9f413d31
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2024-03-29 15:58:10 +0530

    Refactor BooksCard component to include category dropdown

--- src/components/BooksCard.tsx
+++ src/components/BooksCard.tsx
@@ -1,8 +1,27 @@
-import { Book } from "../Constants";
+import { useAppContext } from "../AppProvider";
+import { Book, CATEGORIES_DROPDOWN_OPTIONS } from "../Constants";
 
 const BooksCard = ({ book }: { book: Book }) => {
+  const { setBooksDetails } = useAppContext();
+
+  const generateCategoriesOptions = () => {
+    return CATEGORIES_DROPDOWN_OPTIONS.map(({ label, value }, index) => (
+      <option key={index} value={value}>
+        {label}
+      </option>
+    ));
+  };
+
+  const onCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBooksDetails((books: Book[]) =>
+      books.map((b) =>
+        b.id === book.id ? { ...b, category: e.target.value } : b
+      )
+    );
+  };
+
   return (
-    <div className="flex-shrink-0 mr-4 min-w-[200px] max-w-[200px]">
+    <div className="flex-shrink-0 mr-4 w-56 max-w-[200px]">
       <div className="bg-white p-4 shadow-md rounded-md h-full flex flex-col">
         {book.imageLink && (
           <img
@@ -12,8 +31,17 @@ const BooksCard = ({ book }: { book: Book }) => {
           />
         )}
         <div className="flex-grow">
-          <h3 className="text-lg font-semibold">{book.title}</h3>
-          <p className="text-gray-600">{book.author}</p>
+          <h3 className="text-lg font-semibold mb-2">{book.title}</h3>
+          <p className="text-gray-600 mb-4">{book.author}</p>
+        </div>
+        <div>
+          <select
+            className="py-2 px-4 rounded-md bg-gray-100 w-full"
+            defaultValue={book.category}
+            onChange={onCategoryChange}
+          >
+            {generateCategoriesOptions()}
+          </select>
         </div>
       </div>
     </div>
